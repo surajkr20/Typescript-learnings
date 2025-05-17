ff2ca376d57d4b918e97ee36c7617704799d2b6d
surajkr20 <suraj71308kumar@gmail.com>
2025-05-17 09:37:17 +0530

    revision

--- src/class.ts
+++ src/class.ts
@@ -2,42 +2,43 @@
 type nameType = (name: string, height: number, weight: number) => number | string;
 
 class Player{
+    public readonly userId: string | undefined = String(Math.random()*100);
     private height;
     public weight;
-    protected power;
-    public readonly id:number;
 
-    constructor(height:number, weight: number, power: number){
+    AccessHeight(){
+        return this.height;
+    }
+
+    constructor(height: number, weight: number, userId?: string){
         this.height = height;
-        this.weight = height;
-        this.power = power;
-        this.id = Number(Math.random() * 100);
-    }
-
-    MyHeight = () =>{ // accessing private variables using fun
-        return this.height;
-    }
-
-    get GetMyHeight():number{   // getter function
-        return this.height;
-    }
-
-    set ChangeHeight(val: number){  // setter function
-        this.height = val;
+        this.weight = weight;
+        this.userId = userId;
     }
 }
 
-class Plater extends Player{
-    public name;
-    constructor(name: number){
-        super(11, 34, 69, );
+class NewPlayer extends Player{
+    name: string;
+
+    constructor(height: number, weight: number, name: string){
+        super(height, weight);
         this.name = name;
     }
+
+    GetMyId(){
+        this.userId;
+    }
 }
 
-const suraj = new Player(5.4, 36, 25);
+const suraj = new Player(12.43, 6.5, "12st435");
 
-console.log(suraj.id);
+// console.log(suraj.weight)
+
+// console.log(suraj.AccessHeight())
+
+const abhinav = new NewPlayer(12, 23, "surajkr");
+
+console.log(abhinav.userId)
 
 
 
